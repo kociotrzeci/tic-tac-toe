2b20ead03dbb363c09da46af011db30753c9ebad
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2024-01-11 21:29:37 +0100

    win condition stops game

--- script.js
+++ script.js
@@ -7,107 +7,110 @@ game.reset();
 
 function Board() {
   const board = new Array(boardSize).fill().map(() => new Array(boardSize).fill('.'));
-  function fill (row, col, mark){
-    if (board[col][row]==='.')board[col][row] = mark;
-    else {console.log("invalid move");
-    return 1;}
+  let isWon = false;
+  function fill(row, col, mark) {
+    if (isWon) return 2;
+    if (board[col][row] === '.') board[col][row] = mark;
+    else {
+      console.log("invalid move");
+      return 1;
+    }
   }
-  function logBoard(){
-    for(let i = 0; i < boardSize; i++){
+  function logBoard() {
+    for (let i = 0; i < boardSize; i++) {
       console.log(board[i].join(' '));
     }
     console.log('');
   }
-  function checkRows(mark){
-    for(let i = 0; i <= boardSize - howManyTicks; i++){
-      for(let j = 0; j <= boardSize - howManyTicks; j++){
+  function checkRows(mark) {
+    for (let i = 0; i <= boardSize - howManyTicks; i++) {
+      for (let j = 0; j <= boardSize - howManyTicks; j++) {
         let counter = 0;
-        for(let k = 0; k < howManyTicks; k++){
-          if(board[i][j+k] === mark) counter++;
+        for (let k = 0; k < howManyTicks; k++) {
+          if (board[i][j + k] === mark) counter++;
         }
-        if(counter === howManyTicks) return true;
+        if (counter === howManyTicks) return true;
       }
     }
     return false;
   }
-  function checkColumns(mark){
-    for(let i = 0; i <= boardSize - howManyTicks; i++){
-      for(let j = 0; j <= boardSize - howManyTicks; j++){
+  function checkColumns(mark) {
+    for (let i = 0; i <= boardSize - howManyTicks; i++) {
+      for (let j = 0; j <= boardSize - howManyTicks; j++) {
         let counter = 0;
-        for(let k = 0; k < howManyTicks; k++){
-          if(board[i+k][j] === mark) counter++;
+        for (let k = 0; k < howManyTicks; k++) {
+          if (board[i + k][j] === mark) counter++;
         }
-        if(counter === howManyTicks) return true;
+        if (counter === howManyTicks) return true;
       }
     }
     return false;
   }
-  function checkDiagonalRight(mark){
-    for(let i = 0; i <= boardSize - howManyTicks; i++){
-      for(let j = 0; j <= boardSize - howManyTicks; j++){
+  function checkDiagonalRight(mark) {
+    for (let i = 0; i <= boardSize - howManyTicks; i++) {
+      for (let j = 0; j <= boardSize - howManyTicks; j++) {
         let counter = 0;
-        for(let k = 0; k < howManyTicks; k++){
-          if(board[i+k][j+k] === mark) counter++;
+        for (let k = 0; k < howManyTicks; k++) {
+          if (board[i + k][j + k] === mark) counter++;
         }
-        if(counter === howManyTicks) return true;
+        if (counter === howManyTicks) return true;
       }
     }
     return false;
   }
-  
-  function checkDiagonalLeft(mark){
-    for(let i = 0; i <= boardSize - howManyTicks; i++){
-      for(let j = boardSize - 1; j >= howManyTicks - 1; j--){
+  function checkDiagonalLeft(mark) {
+    for (let i = 0; i <= boardSize - howManyTicks; i++) {
+      for (let j = boardSize - 1; j >= howManyTicks - 1; j--) {
         let counter = 0;
-        for(let k = 0; k < howManyTicks; k++){
-          if(board[i+k][j-k] === mark) counter++;
+        for (let k = 0; k < howManyTicks; k++) {
+          if (board[i + k][j - k] === mark) counter++;
         }
-        if(counter === howManyTicks) return true;
+        if (counter === howManyTicks) return true;
       }
     }
     return false;
   }
-  function doIwin(mark){
-    if (checkRows(mark)) console.log(mark + " wins");
-    if (checkColumns(mark)) console.log(mark + " wins");
-    if (checkDiagonalRight(mark)) console.log(mark + " wins");
-    if (checkDiagonalLeft(mark)) console.log(mark + " wins");
+  function doIwin(mark) {
+    if (checkRows(mark) || checkColumns(mark) || checkDiagonalRight(mark) || checkDiagonalLeft(mark)) {
+      console.log(mark + " wins");
+      return 1;
+    };
   }
-  return {fill, logBoard, doIwin};
+  return { fill, logBoard, doIwin };
 }
 
-function Player(mark, gameBoard){
-  function makeMove(x, y){
-    if (gameBoard.fill(x, y, mark)===1) return 1;
+function Player(mark, gameBoard) {
+  function makeMove(x, y) {
+    if (gameBoard.isWon) return 2;
+    if (gameBoard.fill(x, y, mark)) return 1;
     gameBoard.logBoard();
-    gameBoard.doIwin(mark);    
+    if (gameBoard.doIwin(mark)) gameBoard.isWon = 1;
   }
-  return{makeMove}
+  return { makeMove }
 }
-function Game(){
+function Game() {
   let whooseTurn = true;
   const gameBoard = Board();
   this.p1 = new Player('O', gameBoard);
   this.p2 = new Player('X', gameBoard);
-  this.makeMove = function (cell){
-    if (whooseTurn === true){
+  this.makeMove = function (cell) {
+    if (whooseTurn === true) {
       if (this.p1.makeMove(cell.x, cell.y)) return 1;
-      else  cell.dot.classList.add('white');
+      else cell.dot.classList.add('white');
     }
     else {
       if (this.p2.makeMove(cell.x, cell.y)) return 1;
-      else  cell.dot.classList.add('black');
+      else cell.dot.classList.add('black');
     }
     whooseTurn = !whooseTurn;
   }
   this.reset = function () {
     const container = document.querySelector('.container');
     const game = this;
-    for (let i = 0; i < boardSize; i++){
-      const row =  document.createElement('div')
+    for (let i = 0; i < boardSize; i++) {
+      const row = document.createElement('div')
       row.className = 'row';
-      for(let j = 0; j< boardSize; j++)
-      {
+      for (let j = 0; j < boardSize; j++) {
         const cell = document.createElement('div');
         cell.className = 'cell';
         cell.x = j;
@@ -115,11 +118,11 @@ function Game(){
         row.appendChild(cell);
         cell.dot = document.createElement('span');
         cell.appendChild(cell.dot);
-        cell.addEventListener('click', function(e){
+        cell.addEventListener('click', function (e) {
           game.makeMove(this);
         });
       }
-    container.appendChild(row);
+      container.appendChild(row);
     }
   }
 }
